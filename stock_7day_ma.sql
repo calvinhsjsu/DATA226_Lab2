e40070a30351f079c46cdce1a670b120e67077d6
sakethnbr <52720554+sakethnbr@users.noreply.github.com>
2025-11-25 13:06:11 -0800

    Include volume in stock 7-day moving average query
    Added volume parameter to the stock price query.

--- stock_7day_ma.sql
+++ stock_7day_ma.sql
@@ -5,27 +5,8 @@ WITH raw_data AS (
 SELECT  
     symbol, 
     date, 
-    close, 
-    -- Calculate 7-day moving average
-    AVG(close) OVER (
-        PARTITION BY symbol
-        ORDER BY date
-        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-    ) AS stock_7d_moving_avg,
-FROM raw_data
-ORDER BY date DESC
-
--- BELOW IS THE QUERY WITH THE SAME CODE AS ABOVE EXCEPT ADDED THE VOLUME PARAMETER
-
-WITH raw_data AS (
-    SELECT * FROM {{ ref('stock_prices')}}
-)
-
-SELECT  
-    symbol, 
-    date, 
     close,
-    volume,  -- ⭐ ADDED: Volume for your "Volume Over Time" chart
+    volume, 
     -- Calculate 7-day moving average
     AVG(close) OVER (
         PARTITION BY symbol
